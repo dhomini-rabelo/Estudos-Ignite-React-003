809f712466b853e662bbd5d4cd5281ed28d9664a
dhomini-rabelo <dhominisalgado@gmail.com>
2022-11-06 15:49:26 -0300

    up header component

--- projects/stage03/src/pages/Home/components/Header/index.tsx
+++ projects/stage03/src/pages/Home/components/Header/index.tsx
@@ -1,10 +1,10 @@
 export function Header() {
 
   return (
-    <header className="bg-Gray-900 p-[2.5rem 0 7.5rem]">
+    <header className="bg-Gray-900 p-[2.5rem_0_7.5rem]">
       <div className="content mx-auto px-6 flex justify-between items-center">
         <img src="/images/logo.svg" alt="project-logo" />
-        <button>Nova Transação</button>
+        <button className="h-[3.125rem] border-0 bg-Green-500 text-white font-bold p-[0_1.25rem] rounded-md cursor-pointer hover:bg-Green-700">Nova Transação</button>
       </div>
     </header>
   )
