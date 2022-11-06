7df2e7e5dccb7cfb422aedbb83deb9116be4708c
dhomini-rabelo <dhominisalgado@gmail.com>
2022-11-06 15:35:12 -0300

    add header component

--- projects/stage03/src/core/App.tsx
+++ projects/stage03/src/core/App.tsx
@@ -1,9 +1,9 @@
+import { Home } from "../pages/Home";
+
 export function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home />
   )
 }
 
---
+++ projects/stage03/src/pages/Home/components/Header/index.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+
+  return (
+    <header className="bg-Gray-900 p-[2.5rem 0 7.5rem]">
+      <div className="content mx-auto px-6 flex justify-between items-center">
+        <img src="/images/logo.svg" alt="project-logo" />
+        <button>Nova Transação</button>
+      </div>
+    </header>
+  )
+}
+
