09a229da235ff1f4b524a1eb95e2ab443651c626
dhomini-rabelo <dhominisalgado@gmail.com>
2022-11-06 23:03:45 -0300

    add summary

--- projects/stage03/src/pages/Home/components/SummaryContainer/index.tsx
+++ projects/stage03/src/pages/Home/components/SummaryContainer/index.tsx
@@ -1,16 +1,37 @@
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../code/contexts/Transactions'
 
 export function SummaryContainer() {
+  const { transactions } = useContext(TransactionsContext)
+
+  const summary = transactions.reduce(
+    (accumulator, transaction) => {
+      if (transaction.type === 'income') {
+        accumulator.income += transaction.price
+        accumulator.total += transaction.price
+      } else {
+        accumulator.outcome += transaction.price
+        accumulator.total -= transaction.price
+      }
+
+      return accumulator
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
 
   return (
     <section className="w-full mx-auto p-[0_1.25rem] grid grid-cols-3 gap-8 mt-[-5rem]">
-
       <div className="col-span-1 bg-Gray-600 rounded-md p-8">
         <header className="flex items-center justify-between text-Gray-300">
           <span>Entradas</span>
           <ArrowCircleUp size={32} className="text-Green-300" />
         </header>
-        <strong className="block mt-4 text-[2rem]">R$ 17.400,00</strong>
+        <strong className="block mt-4 text-[2rem]">R$ {summary.income}</strong>
       </div>
 
       <div className="col-span-1 bg-Gray-600 rounded-md p-8">
@@ -18,7 +39,7 @@ export function SummaryContainer() {
           <span>Saídas</span>
           <ArrowCircleDown size={32} className="text-Red-300" />
         </header>
-        <strong className="block mt-4 text-[2rem]">R$ 17.400,00</strong>
+        <strong className="block mt-4 text-[2rem]">R$ {summary.outcome}</strong>
       </div>
 
       <div className="col-span-1 bg-Green-700 rounded-md p-8">
@@ -26,9 +47,8 @@ export function SummaryContainer() {
           <span>Total</span>
           <CurrencyDollar size={32} className="text-white" />
         </header>
-        <strong className="block mt-4 text-[2rem]">R$ 17.400,00</strong>
+        <strong className="block mt-4 text-[2rem]">R$ {summary.total}</strong>
       </div>
-
     </section>
   )
 }
