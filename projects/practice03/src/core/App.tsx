6a5490737f8eeb4f2ab349e58d7800fe63e188dd
dhomini-rabelo <dhominisalgado@gmail.com>
2022-11-09 23:49:11 -0300

    add react-router-dom

--- projects/practice03/src/core/App.tsx
+++ projects/practice03/src/core/App.tsx
@@ -1,3 +1,5 @@
+import { RoutesController } from './router'
+
 export function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <RoutesController />
 }
--- projects/practice03/src/core/main.tsx
+++ projects/practice03/src/core/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import '../assets/styles/index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
