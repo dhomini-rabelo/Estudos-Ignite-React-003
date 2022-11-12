e1080ed142f719e380f33eecfd359fd63d291938
dhomini-rabelo <dhominisalgado@gmail.com>
2022-11-12 16:33:50 -0300

    add header to main template

--- projects/practice03/src/layout/templates/Main/index.tsx
+++ projects/practice03/src/layout/templates/Main/index.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from 'react-router-dom'
+import { Div } from './styles'
 
 export function MainTemplate() {
   return (
-    <div id="container">
-      <header>
-        <img src="/images/Logo.svg" alt="project-logo" />
+    <Div.container id="container">
+      <header className="w-full h-72 flex">
+        <img
+          src="/images/Logo.svg"
+          alt="project-logo"
+          className="w-36 h-24 m-auto d-block mt-16"
+        />
       </header>
       <main>
         <Outlet />
       </main>
-    </div>
+    </Div.container>
   )
 }
---
+++ projects/practice03/src/layout/templates/Main/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Div = {
+  container: styled.div`
+    header {
+      background-image: url('/images/Cover.svg');
+    }
+  `,
+}
