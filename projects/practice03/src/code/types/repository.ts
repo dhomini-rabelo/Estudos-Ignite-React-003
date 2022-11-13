f069730d89bd48c5a76afd79f070a89dd9bbade0
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-13 17:24:59 -0300

    add repositories fake data for contruct screens

---
+++ projects/practice03/src/code/types/repository.ts
@@ -0,0 +1,10 @@
+export interface RepositoryType {
+  id: number
+  name: string
+  full_name: string
+  html_url: string
+  description: string
+  default_branch: string
+  created_at: string
+  updated_at: string
+}
