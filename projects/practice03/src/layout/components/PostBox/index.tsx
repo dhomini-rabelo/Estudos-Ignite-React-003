caf11bd0df6e5f8938389576c6b4d798b6391625
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-13 17:25:20 -0300

    use repositories fake data for postbox component

--- projects/practice03/src/layout/components/PostBox/index.tsx
+++ projects/practice03/src/layout/components/PostBox/index.tsx
@@ -1,19 +1,16 @@
+import { RepositoryType } from '../../../code/types/repository'
 import { Div } from './styles'
 
-export function PostBox() {
+export function PostBox({ repository }: { repository: RepositoryType }) {
   return (
     <Div.container className="p-8 cursor-pointer">
-      <div className="flex items-center-justify-between">
-        <strong className="text-xl text-Gray-100">
-          JavaScript data types and data structures
-        </strong>
-        <span className="text-Blue-500 text-sm">06/12/22</span>
+      <div className="flex items-center justify-between">
+        <strong className="text-xl text-Gray-100">{repository.name}</strong>
+        <span className="text-Blue-500 text-sm">
+          {new Date(repository.created_at).toLocaleDateString()}
+        </span>
       </div>
-      <p className="lh-160 mt-5">
-        Programming languages all have built-in data structures, but these often
-        differ from one language to another. This article attempts to list the
-        built-in data structures available in
-      </p>
+      <p className="lh-160 mt-5">{repository.description}</p>
     </Div.container>
   )
 }
--- projects/practice03/src/pages/Home/index.tsx
+++ projects/practice03/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { MyUserData } from '../../code/data'
+import { MyUserData, myUserRepos } from '../../code/data'
 import { PostBox } from '../../layout/components/PostBox'
 import { UserReport } from '../../layout/components/UserReport'
 import { Div } from './styles'
@@ -9,22 +9,23 @@ export function Home() {
       <UserReport user={MyUserData} />
       <Div.inputContainer className="input-container">
         <div className="flex items-center justify-between mt-10 lh-160">
-          <strong className="text-lg text-Gray-200">Publicações</strong>
-          <span className="text-sm text-Blue-500">6 publicações</span>
+          <strong className="text-lg text-Gray-200">Projetos</strong>
+          <span className="text-sm text-Blue-500">
+            {MyUserData.public_repos} projetos
+          </span>
         </div>
         <input
           type="text"
           className="block w-full mt-3 px-4 h-12 bg-Gray-900 lh-160 placeholder:text-Blue-600 rounded-md"
-          placeholder="Buscar conteúdo"
+          placeholder="Buscar por projeto"
         />
       </Div.inputContainer>
       <div className="post-boxes mt-12 grid grid-cols-2 gap-x-8">
-        <div className="col-span-1">
-          <PostBox />
-        </div>
-        <div className="col-span-1">
-          <PostBox />
-        </div>
+        {myUserRepos.map((repo) => (
+          <div className="col-span-1" key={repo.id}>
+            <PostBox repository={repo} />
+          </div>
+        ))}
       </div>
     </main>
   )
