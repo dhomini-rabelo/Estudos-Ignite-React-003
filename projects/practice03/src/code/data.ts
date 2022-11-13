11b5770d2483e9bea12eefa238c7db5527837334
dhomini-rabelo <dhominisalgado@gmail.com>
2022-11-13 16:04:21 -0300

    add fake data for contruct screens

---
+++ projects/practice03/src/code/data.ts
@@ -0,0 +1,13 @@
+export const MyUserData = {
+  login: 'dhomini-rabelo',
+  avatar_url: 'https://avatars.githubusercontent.com/u/86575834?v=4',
+  html_url: 'https://github.com/dhomini-rabelo',
+  repos_url: 'https://api.github.com/users/dhomini-rabelo/repos',
+  name: 'Dhomini Rabelo',
+  company: 'Inspecto Agri',
+  blog: 'https://www.linkedin.com/in/dhomini-rabelo',
+  location: 'São Luís, MA',
+  bio: 'Desenvolvedor web\n dhominirabelo@gmail.com\r\n',
+  public_repos: 27,
+  followers: 4,
+}
---
+++ projects/practice03/src/code/types/user.ts
@@ -0,0 +1,13 @@
+export interface UserDataType {
+  login: string
+  avatar_url: string
+  html_url: string
+  repos_url: string
+  name: string
+  company: string | null
+  blog: string | null
+  location: string | null
+  bio: string
+  public_repos: number
+  followers: number
+}
