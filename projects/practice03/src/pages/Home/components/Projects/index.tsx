d712e60d06b7b2ab578d1a2bc6a386ae76cfbab1
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-15 11:16:43 -0300

    filtering projects

--- projects/practice03/src/pages/Home/components/Projects/index.tsx
+++ projects/practice03/src/pages/Home/components/Projects/index.tsx
@@ -1,26 +1,51 @@
+import { useState } from 'react'
 import { RepositoryType } from '../../../../code/types/repository'
 import { PostBox } from '../../../../layout/components/PostBox'
 import { Div } from './styles'
 
 export function Projects({ userRepos }: { userRepos: RepositoryType[] }) {
+  const [searchText, setSearchText] = useState('')
+  const filteredProjects = userRepos.filter(
+    (repo) =>
+      repo.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      repo.description?.toLowerCase().includes(searchText.toLowerCase()),
+  )
+
+  function handleClearSearchText() {
+    setSearchText('')
+  }
+
   return (
     <>
       <Div.inputContainer className="input-container">
         <div className="flex items-center justify-between mt-10 lh-160">
           <strong className="text-lg text-Gray-200">Projetos</strong>
-          <span className="text-sm text-Blue-500">
-            {userRepos.length} projetos
-          </span>
+          <div className="flex items-center gap-x-2">
+            <span className="text-sm text-Blue-500">
+              {userRepos.length} projetos
+            </span>
+            {searchText && (
+              <span
+                className="text-sm text-Blue-300 cursor-pointer"
+                onClick={handleClearSearchText}
+              >
+                <i className="fa-solid fa-x mr-1"></i>
+                Limpar
+              </span>
+            )}
+          </div>
         </div>
         <input
           type="text"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
           className="block w-full mt-3 px-4 h-12 bg-Gray-900 lh-160 placeholder:text-Blue-600 rounded-md"
           placeholder="Buscar por projeto"
         />
       </Div.inputContainer>
       <div className="post-boxes mt-12 grid grid-cols-2 gap-x-8">
         {userRepos.length > 0 ? (
-          userRepos.map((repo) => (
+          filteredProjects.map((repo) => (
             <div className="col-span-1" key={repo.id}>
               <PostBox repository={repo} />
             </div>
--- projects/practice03/src/pages/Project/styles.ts
+++ projects/practice03/src/pages/Project/styles.ts
@@ -4,11 +4,11 @@ export const Div = {
   header: styled.div`
     margin-top: -7.2rem;
 
-    .github-link {
+    a {
       border-bottom: 1px solid transparent;
     }
 
-    .github-link:hover {
+    a:hover {
       border-bottom: 1px solid var(--Blue-300);
     }
   `,
