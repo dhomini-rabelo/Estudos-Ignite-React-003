8d6c78083e72b110a647eff9d24fa26676b8f009
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2022-11-14 22:45:48 -0300

    add project page header

--- projects/practice03/src/pages/Project/index.tsx
+++ projects/practice03/src/pages/Project/index.tsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom'
 import { Loading } from '../../layout/components/Loading'
+import { Div } from './styles'
 
 export function Project() {
   const isLoading = false
 
-  return !isLoading ? <main>project page</main> : <Loading />
+  return !isLoading ? (
+    <main>
+      <Div.header className="flex items-center justify-between text-Blue-300">
+        <Link to="/" className="lh-160">
+          <i className="fa-solid fa-chevron-left mr-2"></i>
+          <span>VOLTAR</span>
+        </Link>
+        <a className="github-link" target="_blank" rel="noreferrer" href="#">
+          GITHUB
+          <i className="fa-solid fa-arrow-up-right-from-square ml-2"></i>
+        </a>
+      </Div.header>
+      <section>
+        <div>content</div>
+      </section>
+    </main>
+  ) : (
+    <Loading />
+  )
 }
---
+++ projects/practice03/src/pages/Project/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Div = {
+  header: styled.div`
+    margin-top: -7.2rem;
+
+    .github-link {
+      border-bottom: 1px solid transparent;
+    }
+
+    .github-link:hover {
+      border-bottom: 1px solid var(--Blue-300);
+    }
+  `,
+}
